1de3565386440534192cdc7b6bbbc7128df9e046
Alfonso Farías <77316261+chronologie7@users.noreply.github.com>
2021-04-27 20:08:06 -0400

    optimización
    optimización por raiz.

--- primos.cpp
+++ primos.cpp
@@ -1,48 +1,41 @@
 #include <iostream>
-#include <ctime>
-
+#include <cmath>
 using namespace std;
 
 int main()
 {
     int numero;
-    int contador = 0;
+    bool esPrimo = true;
     int primoNumero = 0;
     int calculos = 0;
-    unsigned t0, t1;
 
     cout << "Ingrese un número para el rango de N primos:" << endl;
     cin >> numero;
-
-    t0 = clock();
 
     primoNumero++;
     cout << primoNumero << ". " << 2 << endl;
 
     for (int numeroActual = 3; numeroActual < numero; numeroActual += 2)
     {
-        for (int divisor = 2; divisor < numeroActual; divisor++)
+        for (int divisor = 2; divisor < int(sqrt(numeroActual)) + 1; divisor++)
         {
             calculos++;
 
             if (numeroActual % divisor == 0)
             {
-                contador++;
+                esPrimo = false;
                 break;
             }
         }
-        if (contador == 0)
+        if (esPrimo)
         {
             primoNumero++;
             cout << primoNumero << ". " << numeroActual << endl;
         }
-        contador = 0;
+        esPrimo = true;
     }
 
-    t1 = clock();
-
     cout << "Calculos: " << calculos << endl;
-    double time = (double(t1 - t0) / CLOCKS_PER_SEC);
-    cout << "Tiempo de ejecución: " << time << endl;
+
     return 0;
 }
